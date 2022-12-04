eb9681c15f2d6762a79ff94a570737b6e2ff529a
Eemeli Heinonen <eemeli.heinonen@yonoton.com>
2022-12-04 18:22:22 +0200

    day-01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
+fun sumByElf(input: List<String>): List<Int> = input.fold(mutableListOf(0)) { sums, currentString ->
+    if (currentString == "") {
+        sums.add(0)
+    }
+    else {
+        sums[sums.lastIndex] = sums.last().plus(currentString.toInt())
+    }
+    sums
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return sumByElf(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return sumByElf(input)
+            .sorted()
+            .takeLast(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
